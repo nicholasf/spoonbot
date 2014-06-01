a28476348ae865c237975a9c55bc5059adcb7b0c
nicholasf <nicholas.faiz@gmail.com>
2014-06-01 16:45:51 +1000

    erm, no tests yet

--- spoonbot.exs
+++ spoonbot.exs
@@ -9,3 +9,6 @@ command "heya", fn (speaker) ->
     greeting = Enum.at greetings, round(:random.uniform ((Enum.count greetings) -1))
     "#{greeting} #{speaker}" 
 end
+
+command "die", &("No, you die #{&1}!")
+command "quit", &("Haha, after you #{&1}!")
--- test/spoonbot_test.exs
+++
@@ -1,7 +0,0 @@
-# defmodule SpoonbotTest do
-#   use ExUnit.Case
-
-#   test "the truth" do
-#     assert(true)
-#   end
-# end
