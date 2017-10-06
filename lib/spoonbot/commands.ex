f1f9c846e4508edde73859d7ca749c37f2c2160d
Faiz <faizn1@nca1031460.news.newslimited.local>
2017-10-06 15:35:37 +1100

    Spoonbot upgraded to compile with 1.5.2

--- lib/spoonbot/commands.ex
+++ lib/spoonbot/commands.ex
@@ -2,11 +2,11 @@ defmodule Commands do
     @name { :global, __MODULE__ }
 
     def start_link do
-        Agent.start_link( fn-> HashSet.new end, name: @name )
+        Agent.start_link( fn-> MapSet.new end, name: @name )
     end 
 
     def add(command) do
-        Agent.update(@name, fn(set) -> Set.put(set, command) end)
+        Agent.update(@name, fn(set) -> MapSet.put(set, command) end)
     end
 
     def find(phrase) do
--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Spoonbot.Mixfile do
     [ app: :spoonbot,
       version: "0.0.1",
       elixir: ">= 0.13.3",
-      deps: deps ]
+      deps: deps() ]
   end
 
   # Configuration for the OTP application
